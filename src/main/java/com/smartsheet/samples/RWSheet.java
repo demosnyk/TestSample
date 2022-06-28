8757c8a4f2a6e00709fd8e077154ad0419ed5e02
demosnyk <108069250+demosnyk@users.noreply.github.com>
2022-06-28 13:06:09 +0530

    Update RWSheet.java

--- src/main/java/com/smartsheet/samples/RWSheet.java
+++ src/main/java/com/smartsheet/samples/RWSheet.java
@@ -120,5 +120,22 @@ public class RWSheet {
                 .orElse(null);
 
     }
+    
+    public static void getConn()
+    {
+            String url;  
+            Connection con = null;  
+            try {  
+                Class.forName("com.mysql.jdbc.Driver");  
+                url="jdbc:mysql://localhost:3306/spring";  
+                con = DriverManager.getConnection(url);  
+                System.out.println("Connection created");  
+                con.close();  
+                System.out.println("Connection closed");  
+                }  
+                catch (Exception e) {  
+                System.out.println(e.toString());  
+            }  
+    }
 
 }
